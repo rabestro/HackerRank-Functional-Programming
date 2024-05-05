751e57ae6b0780b70eef124e5df642b21666b33b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-05 21:44:21 +0300

    Add new F# projects and update solution
    Created two new F# projects named 'Hello World' and 'List Replication' and updated the HackerRank solution to include these two projects. In addition, added a '.gitignore' file to exclude specific files and directories from the repository.

---
+++ Hello World/Program.fs
@@ -0,0 +1,5 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let n = System.Console.ReadLine() |> int
+
+for i in 1 .. n do
+    printfn "Hello World"
---
+++ List Replication/Program.fs
@@ -0,0 +1,8 @@
+﻿let n = System.Console.ReadLine() |> int
+
+Seq.initInfinite (fun _ -> System.Console.ReadLine())
+    |> Seq.takeWhile (fun line -> line <> null)
+    |> Seq.collect (Seq.replicate n)
+    |> Seq.iter (printfn "%s")
+    
+    
