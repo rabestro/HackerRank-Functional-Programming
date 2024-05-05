2fbdf3b65b4f2b0bdfa67cb668eabb011ce3ed56
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-05 21:54:41 +0300

    Add "Filter Array" project to the solution
    A new project named "Filter Array" has been added to the HackerRank solution. This includes the project configuration in the .fsproj file and the implementation in the Program.fs file. Also, the solution file has been updated with the new project details including the respective build and release configurations.

---
+++ Filter Array/Program.fs
@@ -0,0 +1,7 @@
+﻿let limit = System.Console.ReadLine() |> int
+
+Seq.initInfinite (fun _ -> System.Console.ReadLine())
+|> Seq.takeWhile (fun line -> line <> null)
+|> Seq.map int
+|> Seq.filter (fun n -> n < limit)
+|> Seq.iter (printfn "%i")
