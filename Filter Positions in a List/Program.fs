b0e8931aded1cbece1fb7dc14e2c5cccbb2e7809
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-05 22:09:32 +0300

    Add 'Filter Positions in a List' solution and integrate with the solution file.
    The commit includes creating a new F# program 'Filter Positions in a List', and integrating the program into the existing solution file 'HackerRank.sln'. The new program will sequentially read lines of input and output the ones in even positions.
    
    @coderabbitai ignore

---
+++ Filter Positions in a List/Program.fs
@@ -0,0 +1,7 @@
+﻿let removeOdd i x = if i % 2 = 1 then Some x else None
+
+Seq.initInfinite (fun _ -> System.Console.ReadLine())
+|> Seq.takeWhile (fun line -> line <> null)
+|> Seq.mapi removeOdd
+|> Seq.choose id
+|> Seq.iter (printfn "%s")
