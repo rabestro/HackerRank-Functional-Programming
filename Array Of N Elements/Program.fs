f8c0e395345d4ba745ec1e1d25e2d317f8026220
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-06 11:34:53 +0300

    Add "Array Of N Elements" project to solution
    Added a new project "Array Of N Elements" to the solution and updated the solution's configuration to include build settings for this new project. The program creates an array of n integers, where n is passed as an argument.
    
    @coderabbitai ignore

---
+++ Array Of N Elements/Program.fs
@@ -0,0 +1,16 @@
+﻿(*
+    Create an array of integers, where the value of is passed as an argument to the pre-filled function in your editor. 
+    This challenge uses a custom checker, so you can create any array of n integers. 
+    For example, if , n = 4 you could return [1, 2, 3, 4], [1, 1, 1, 1], or any other array of equal length.
+*)
+
+open System
+
+let f n : int[] = Array.zeroCreate n
+
+let main () =
+    let input = Console.ReadLine()
+    let n = int input
+    printfn $"%A{f n}"
+
+main ()
